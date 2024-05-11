20d47d621359ee2b1ffe8014309215bf16f2bf3e
dev99 <arssw71@gmail.com>
2024-05-11 22:15:35 +0330

    add new action for set todos

--- src/components/Todo/TodoActions.ts
+++ src/components/Todo/TodoActions.ts
@@ -2,6 +2,7 @@ import {TodoState} from './TodoState';
 
 export enum TypeKeys {
   ADD_TODO = 'ADD_TODO',
+  ADD_ALL_TODOS = 'ADD_ALL_TODOS',
   REMOVE_TODO = 'REMOVE_TODO',
 }
 
@@ -9,17 +10,28 @@ export type AddTodoAction = {
   type: TypeKeys.ADD_TODO;
   todo: TodoState;
 };
+export type AddALLTodoAction = {
+  type: TypeKeys.ADD_ALL_TODOS;
+  todos: TodoState[];
+};
 
 export type RemoveTodoAction = {
   type: TypeKeys.REMOVE_TODO;
   index: number;
 };
 
-export type TodoActionTypes = AddTodoAction | RemoveTodoAction;
+export type TodoActionTypes =
+  | AddTodoAction
+  | RemoveTodoAction
+  | AddALLTodoAction;
 
 export const addTodoAction = (todo: TodoState) => ({
   type: TypeKeys.ADD_TODO,
   todo,
+});
+export const addAllTodosAction = (todos: TodoState[]) => ({
+  type: TypeKeys.ADD_ALL_TODOS,
+  todos,
 });
 export const removeTodoAction = (index: string) => ({
   type: TypeKeys.REMOVE_TODO,
