752e0b3ebcef3b55934e9ba933a1159adce6e20a
dev99 <arssw71@gmail.com>
2024-05-11 23:50:17 +0330

    adding some simple styles

--- src/components/Todo/TodoData.tsx
+++ src/components/Todo/TodoData.tsx
@@ -1,6 +1,8 @@
 import {useEffect} from 'react';
 import {useTodo} from '@hooks/useTodo';
 import {getTodoListAPI} from '@services/todo';
+import './style.css';
+
 function TodoData() {
   const {todos, removeTodo, addAllTodos} = useTodo();
   const data = todos ?? [];
@@ -18,7 +20,11 @@ function TodoData() {
         data.length &&
         data.map((todo, index) => (
           <li className="item" key={todo.id}>
-            {todo.title}
+            <span className="title">{todo.title}</span>
+            <span className="description">{todo.description}</span>
+            <span className={todo.completed ? 'completed' : 'not-completed'}>
+              {todo.completed ? 'completed' : 'not completed'}
+            </span>
             <button onClick={() => removeTodo(index)}>Delete</button>
           </li>
         ))}
