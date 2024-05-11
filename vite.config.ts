4495ca0239f602c588ce7375c3b4e2b2b9b16812
dev99 <arssw71@gmail.com>
2024-05-11 22:11:58 +0330

    fix config for path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
 import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
+import {resolve} from 'path';
 
+// https://vitejs.dev/config/
+
+const root = resolve(__dirname, 'src');
+const outDir = resolve(__dirname, 'public');
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: outDir,
+  },
+  resolve: {
+    alias: {
+      '@components': resolve(root, 'components'),
+      '@hooks': resolve(root, 'hooks'),
+      '@store': resolve(root, 'store'),
+      '@services': resolve(root, 'services'),
+      '@Errors': resolve(root, 'Errors'),
+    },
+  },
 });
