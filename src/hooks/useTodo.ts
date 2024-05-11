9972b726cea31395789dccc7292b6e6883586d2a
dev99 <arssw71@gmail.com>
2024-05-11 23:15:19 +0330

    get data from json-server and dispatch to state manager

--- src/hooks/useTodo.ts
+++ src/hooks/useTodo.ts
@@ -2,26 +2,35 @@ import {useCallback} from 'react';
 import {useSelector, useDispatch} from 'react-redux';
 import {RootState} from '../store/RootState';
 import {TodoState} from '../components/Todo/TodoState';
-import {removeTodoAction, addTodoAction} from '../components/Todo/TodoActions';
+import {
+  removeTodoAction,
+  addTodoAction,
+  addAllTodosAction,
+} from '../components/Todo/TodoActions';
 
 type UseTodoProps = {
-  todos: TodoState[];
-  removeTodo: (id: string) => void;
+  todos: TodoState;
+  removeTodo: (index: number) => void;
   addTodo: (todo: TodoState) => void;
+  addAllTodos: (todos: RootState) => void;
 };
 
 export const useTodo = (): UseTodoProps => {
   const dispatch = useDispatch();
 
-  const todos: TodoState[] = useSelector((state: RootState) => state.todos);
+  const todos: TodoState = useSelector((state: RootState) => state.todos.todos);
   const removeTodo = useCallback(
-    (id: string) => dispatch(removeTodoAction(id)),
+    (index: number) => dispatch(removeTodoAction(index)),
     [dispatch]
   );
   const addTodo = useCallback(
     (todo: TodoState) => dispatch(addTodoAction(todo)),
     [dispatch]
   );
+  const addAllTodos = useCallback(
+    (todos: RootState) => dispatch(addAllTodosAction(todos)),
+    [dispatch]
+  );
 
-  return {todos, removeTodo, addTodo};
+  return {todos, removeTodo, addTodo, addAllTodos};
 };
